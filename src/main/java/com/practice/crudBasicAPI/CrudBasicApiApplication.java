38d4907f825e9f873cb017dd2c4fedb347675718
ian.srbootcamp <ian.srbootcamp@gmail.com>
2023-10-26 10:48:56 +0800

    First Commit

---
+++ src/main/java/com/practice/crudBasicAPI/CrudBasicApiApplication.java
@@ -0,0 +1,13 @@
+package com.practice.crudBasicAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/crudBasicAPI/CrudBasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.crudBasicAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
